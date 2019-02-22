f85b13752848eb60ab78c1635aaaf1c50fd3e225
Eric Mansfield <ericman314@gmail.com>
2019-02-21 20:14:44 -0700

    Various visual improvements

--- rocket/falcon.py
+++ rocket/falcon.py
@@ -5,11 +5,11 @@ import math
 
 def loadFalcon(nodePath, radius, height):
 
-  uSteps = 16
-  vSteps = 32
+  uSteps = 32
+  vSteps = 64
   
   falconData = GeomVertexData('falcon', GeomVertexFormat.getV3n3t2(), Geom.UHStatic)
-  falconData.setNumRows(4)
+  # falconData.setNumRows(4)
   falconDataVertexWriter = GeomVertexWriter(falconData, 'vertex')
   falconDataNormalWriter = GeomVertexWriter(falconData, 'normal')
   falconDataTexcoordWriter = GeomVertexWriter(falconData, 'texcoord')
@@ -19,14 +19,34 @@ def loadFalcon(nodePath, radius, height):
   i = 0
 
   for u in range(uSteps):
-    x0 = math.cos(2 * math.pi * u / uSteps) * radius
-    y0 = math.sin(2 * math.pi * u / uSteps) * radius
-    x1 = math.cos(2 * math.pi * (u+1) / uSteps) * radius
-    y1 = math.sin(2 * math.pi * (u+1) / uSteps) * radius
+    y0 = math.sin(2 * math.pi * u / uSteps)
+    x0 = math.cos(2 * math.pi * u / uSteps)
+    x1 = math.cos(2 * math.pi * (u+1) / uSteps)
+    y1 = math.sin(2 * math.pi * (u+1) / uSteps)
 
-    for v in range(vSteps):
-      z0 = 1.0 * v / vSteps * height - height/2
-      z1 = 1.0 * (v+1) / vSteps * height - height/2
+    falconDataVertexWriter.addData3f(x0*radius, y0*radius, -height*0.5)
+    falconDataVertexWriter.addData3f(x1*radius, y1*radius, -height*0.5)
+    falconDataVertexWriter.addData3f(0, 0, -height*0.5)
+
+    falconDataVertexWriter.addData3f(x0*radius, y0*radius, height*0.5)
+    falconDataVertexWriter.addData3f(x1*radius, y1*radius, height*0.5)
+    falconDataVertexWriter.addData3f(0, 0, height*0.5)
+
+    falconDataNormalWriter.addData3f(0, 0, -1)
+    falconDataNormalWriter.addData3f(0, 0, -1)
+    falconDataNormalWriter.addData3f(0, 0, -1)
+
+    falconDataNormalWriter.addData3f(0, 0, 1)
+    falconDataNormalWriter.addData3f(0, 0, 1)
+    falconDataNormalWriter.addData3f(0, 0, 1)
+
+    prim.addVertices(i, i+2, i+1)
+    prim.addVertices(i+3, i+4, i+5)
+    i = i + 6
+
+    for v in range(0, vSteps):
+      z0 = 1.0 * v / vSteps * height - height*0.5
+      z1 = 1.0 * (v+1) / vSteps * height - height*0.5
 
       falconDataVertexWriter.addData3f(x0*radius, y0*radius, z0)
       falconDataVertexWriter.addData3f(x0*radius, y0*radius, z1)
@@ -48,6 +68,7 @@ def loadFalcon(nodePath, radius, height):
       prim.addVertices(i, i+3, i+2)
       i = i + 4
 
+
   prim.close_primitive()
 
   geom = Geom(falconData)
@@ -68,9 +89,10 @@ def loadFalcon(nodePath, radius, height):
   path.reparentTo(nodePath)
 
   myMaterial = Material()
-  myMaterial.setDiffuse(1.0) #Make this material shiny
-  myMaterial.setAmbient(0.2)
-  myMaterial.setAmbient((0.9, 0.9, 0.9, 1))
+  myMaterial.setDiffuse(0.8)
+  myMaterial.setAmbient((0.6,0.6,0.6,1))
+  myMaterial.setSpecular(0.8)
+  myMaterial.setShininess(4)
   
   path.setMaterial(myMaterial) #Apply the material to this nodePath
   
--- rocket/simulation.py
+++ rocket/simulation.py
@@ -2,7 +2,7 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
-from panda3d.core import Point3, Vec3, ConfigVariableDouble, DirectionalLight, AmbientLight, VBase4
+from panda3d.core import Point3, Vec3, ConfigVariableDouble, ConfigVariableInt, DirectionalLight, AmbientLight, VBase4, LVector3f, AntialiasAttrib
 from panda3d.bullet import BulletWorld, BulletCylinderShape, BulletPlaneShape, BulletRigidBodyNode, Z_up
 
 import imagery
@@ -11,6 +11,8 @@ import falcon
 from math import pi, sin, cos
  
 ConfigVariableDouble('default-far').setValue(20000000)
+ConfigVariableInt('framebuffer-multisample').setValue(1)
+ConfigVariableInt('multisamples').setValue(2)
 
 class MyApp(ShowBase):
  
@@ -33,15 +35,31 @@ class MyApp(ShowBase):
     falconHeight = 47
     rocketNode = BulletRigidBodyNode('Rocket')
     rocketNode.setMass(1.0)   # TODO: Update this, determine how Bullet reacts if the mass changes
-    rocketNode.addShape(BulletCylinderShape(1.8542, 47, Z_up))   # radius, height, axis
-    
-    # NOTE: Possible future need to save return valve from attachNewNode
+    rocketNode.addShape(BulletCylinderShape(falconRadius, falconHeight, Z_up))   # radius, height, axis
+
     rocketNP = self.render.attachNewNode(rocketNode)
-    rocketNP.setPos(0, 0, 100)
+    
+    # Set initial position/velocity
+    rocketNP.setPos(0, 0, 60)
+    rocketNode.set_linear_velocity(LVector3f(0, -10, 0))
+    rocketNode.set_angular_velocity(LVector3f(2, 0, 0))
+
+    # Turn off damping, sleeping, and other bells and whistles
+    rocketNode.set_linear_sleep_threshold(0)
+    rocketNode.set_angular_sleep_threshold(0)
+    rocketNode.set_linear_damping(0)
+    rocketNode.set_angular_damping(0)
+    # rocketNode.set_friction(0)
+
+    # Make the rocket bounce off the ground for fun
+    rocketNode.set_restitution(0.8)
+    groundNode.set_restitution(0.8)
+
 
     self.world.attachRigidBody(groundNode)
     self.world.attachRigidBody(rocketNode)
 
+  
 
     # Load models
     imagery.loadScenery(self.render.attachNewNode('sceneryNode'))
@@ -53,13 +71,15 @@ class MyApp(ShowBase):
     dlight = DirectionalLight('dlight')
     dlight.setColor(VBase4(0.8, 0.8, 0.8, 1))
     dlnp = self.render.attachNewNode(dlight)
-    dlnp.setHpr(0, -60, 0)
+    dlnp.setHpr(30, -60, 0)
     self.render.setLight(dlnp)
 
     alight = AmbientLight('alight')
-    alight.setColor(VBase4(0.2, 0.2, 0.2, 1))
+    alight.setColor(VBase4(0.3, 0.3, 0.3, 1))
     alnp = self.render.attachNewNode(alight)
     self.render.setLight(alnp)
+
+    self.render.setAntialias(AntialiasAttrib.MAuto)
 
     # Add the spinCameraTask procedure to the task manager.
     self.taskMgr.add(self.tick, "Tick")
@@ -76,9 +96,9 @@ class MyApp(ShowBase):
     angleDegrees = task.time * 5.0
     angleRadians = angleDegrees * (pi / 180.0)
     # dist = 1 * 1.5 ** (-10 * cos(task.time / 10) + 20)
-    dist = 250
-    self.camera.setPos(dist * sin(angleRadians), -dist * cos(angleRadians), dist*0.5)
-    self.camera.setHpr(angleDegrees, -25, 0)
+    dist = 150
+    self.camera.setPos(dist * sin(angleRadians), -dist * cos(angleRadians), dist*0.4)
+    self.camera.setHpr(angleDegrees, -15, 0)
     
     return Task.cont    # Execute the task again
 

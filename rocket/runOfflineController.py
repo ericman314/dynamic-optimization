f876d7d3804776387bde71ec2b5a4f572a981d1e
Eric Mansfield <ericman314@gmail.com>
2019-04-13 10:28:15 -0600

    Most recent updates

--- rocket/runOfflineController.py
+++ rocket/runOfflineController.py
@@ -2,11 +2,33 @@ from model import getModel
 import numpy as np
 import matplotlib.pyplot as plt
 
-v = [ 6.17830566e+03,  3.50692603e+03,  3.37449648e+04, -1.48535400e+02,
- -8.92251434e+01, -7.96663025e+02,  1.83035949e-01, 1.13647645e-01,
-  1.99493699e-06,  4.48860824e-06,  2.08500000e+04]
+initData = np.loadtxt('offlineController init.csv', delimiter=',').transpose()
 
 mpc = getModel(name='mpc-simple')
+mpc.time = np.linspace(0, 70, 71)
+
+mpc.x.VALUE = initData[1,:]
+mpc.y.VALUE = initData[2,:]
+mpc.z.VALUE = initData[3,:]
+mpc.vx.VALUE = initData[4,:]
+mpc.vy.VALUE = initData[5,:]
+mpc.vz.VALUE = initData[6,:]
+mpc.propMass.VALUE = initData[7,:]
+mpc.Yaw.VALUE = initData[8,:]
+mpc.Pitch.VALUE = initData[9,:]
+
+
+# mpc.x.VALUE = initData[1,0]
+# mpc.y.VALUE = initData[2,0]
+# mpc.z.VALUE = initData[3,0]
+# mpc.vx.VALUE = initData[4,0]
+# mpc.vy.VALUE = initData[5,0]
+# mpc.vz.VALUE = initData[6,0]
+# mpc.propMass.VALUE = initData[7,0]
+# mpc.Yaw.VALUE = initData[8,0]
+# mpc.Pitch.VALUE = initData[9,0]
+
+
 
 mpc.options.IMODE = 6
 mpc.options.MAX_ITER = 500
@@ -19,16 +41,6 @@ mpc.vy.FSTATUS = 1
 mpc.vz.FSTATUS = 1
 mpc.propMass.FSTATUS = 1
 
-mpc.time = np.linspace(0, 70, 71)
-
-# Set initial conditions
-mpc.x.VALUE = v[0]
-mpc.y.VALUE = v[1]
-mpc.z.VALUE = v[2]
-mpc.vx.VALUE = v[3]
-mpc.vy.VALUE = v[4]
-mpc.vz.VALUE = v[5]
-mpc.propMass.VALUE = v[10]
 
 # mpc.EngineOn.STATUS = 1
 mpc.Throttle.STATUS = 1
@@ -46,20 +58,40 @@ finalMask = mpc.Param(_finalMask)
 # yCrash = mpc.Intermediate(mpc.y + mpc.vy * tCrash)
 
 mpc.Obj( (mpc.vz**2 + mpc.z**2) * finalMask  )
-mpc.Obj( mpc.x**2 * finalMask  )
-mpc.Obj( mpc.y**2 * finalMask  )
+mpc.Obj( (mpc.vx**2 + mpc.x**2) * finalMask  )
+mpc.Obj( (mpc.vy**2 + mpc.y**2) * finalMask  )
 
 # Maybe we'll dry a different objective that keeps the rocket on a ballistic trajectory towards the origin, instead of just the final point.
 
 mpc.options.RTOL = 1e0
 mpc.options.OTOL = 1e-2
 
-mpc.options.COLDSTART = 1
+# mpc.options.COLDSTART = 1
+
+# mpc.solve()
+
 
 mpc.solve()
 
+# mpc.solve()
 
-mpc.solve()
+# Time (sec), X (m), Y (m), Z (m), Xdot (m/s), Ydot (m/s), Zdot (m/s), PropMass (kg), Throttle (0-1), Yaw (deg), Pitch (deg)
+data = np.vstack((
+  mpc.time,
+  mpc.x,
+  mpc.y,
+  mpc.z,
+  mpc.vx,
+  mpc.vy,
+  mpc.vz,
+  mpc.propMass,
+  mpc.Throttle,
+  mpc.Yaw,
+  mpc.Pitch
+))
+
+top = '# Time (sec), X (m), Y (m), Z (m), Xdot (m/s), Ydot (m/s), Zdot (m/s), PropMass (kg), Throttle (0-1), Yaw (deg), Pitch (deg)'
+np.savetxt('offlineController output.csv', data, fmt='%.4f', delimiter=', ', header=top)
 
 
 plt.figure(figsize=(11,8))
@@ -86,4 +118,6 @@ plt.plot(mpc.time, mpc.Yaw, 'r-', label='Yaw')
 plt.plot(mpc.time, mpc.Pitch, 'b-', label='Pitch')
 plt.legend()
 
+plt.savefig('offlineController output.png')
 plt.show()
+

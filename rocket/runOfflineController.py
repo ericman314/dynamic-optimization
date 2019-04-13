Updates to runOfflineController, minor fix with the sign of pitch

rocket/runOfflineController.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 v = [ 6.17830566e+03,  3.50692603e+03,  3.37449648e+04, -1.48535400e+02,
- -8.92251434e+01, -7.96663025e+02,  1.83035949e-01, -1.13647645e-01,
+ -8.92251434e+01, -7.96663025e+02,  1.83035949e-01, 1.13647645e-01,
   1.99493699e-06,  4.48860824e-06,  2.08500000e+04]
 
 mpc = getModel(name='mpc')
 
 mpc.options.IMODE = 6
+mpc.options.MAX_ITER = 500
 
 mpc.x.FSTATUS = 1
 mpc.y.FSTATUS = 1
@@ -39,17 +40,20 @@ mpc.propMass.VALUE = v[10]
 
 mpc.EngineOn.STATUS = 1
 mpc.Throttle.STATUS = 1
+mpc.Gridx.STATUS = 1
+# mpc.Gridy.STATUS = 1
 
 _finalMask = np.zeros(mpc.time.size)
 _finalMask[-1] = 1
 finalMask = mpc.Param(_finalMask)
 
-tCrash = mpc.Intermediate((mpc.vz + mpc.sqrt(mpc.vz**2 + 2*mpc.g*mpc.z)) / mpc.g)
-xCrash = mpc.Intermediate(mpc.x + mpc.vx * tCrash)
-yCrash = mpc.Intermediate(mpc.y + mpc.vy * tCrash)
+# tCrash = mpc.Intermediate((mpc.vz + mpc.sqrt(mpc.vz**2 + 2*mpc.g*mpc.z)) / mpc.g)
+# xCrash = mpc.Intermediate(mpc.x + mpc.vx * tCrash)
+# yCrash = mpc.Intermediate(mpc.y + mpc.vy * tCrash)
 
 mpc.Obj( (mpc.vz**2 + mpc.z**2) * finalMask  )
-# mpc.Obj( mpc.x**2 * finalMask  )
+mpc.Obj( mpc.x**2 * finalMask  )
+# mpc.Obj( mpc.y**2 * finalMask  )
 
 # Maybe we'll dry a different objective that keeps the rocket on a ballistic trajectory towards the origin, instead of just the final point.
 
@@ -57,14 +61,8 @@ mpc.options.RTOL = 1e-2
 mpc.options.OTOL = 1e-2
 
 mpc.options.COLDSTART = 1
-mpc.options.REDUCE = 10
 
 mpc.solve()
-
-mpc.solve()
-
-# mpc.Gridx.STATUS = 1
-# mpc.Gridy.STATUS = 1
 
 
 mpc.solve()
@@ -72,23 +70,32 @@ mpc.solve()
 
 plt.figure(figsize=(11,8))
 plt.subplot(2, 3, 1)
-plt.plot(mpc.time, mpc.z.value)
+plt.plot(mpc.time, mpc.z.value, label='z')
+plt.legend()
 plt.subplot(2, 3, 2)
-plt.plot(mpc.time, mpc.vz.value)
+plt.plot(mpc.time, mpc.vz.value, label='vz')
+plt.legend()
 plt.subplot(2, 3, 3)
-plt.plot(mpc.time, mpc.Throttle)
-plt.plot(mpc.time, mpc.EngineOn)
+plt.plot(mpc.time, mpc.Throttle, label='Throttle')
+plt.plot(mpc.time, mpc.EngineOn, label='EngineOn')
+plt.legend()
 plt.subplot(2, 3, 4)
-plt.plot(mpc.time, mpc.x, 'r-')
-plt.plot(mpc.time, mpc.y, 'b-')
-plt.plot(mpc.time, mpc.vx, 'r.')
-plt.plot(mpc.time, mpc.vy, 'b.')
-plt.plot(mpc.time, xCrash, 'r:')
-plt.plot(mpc.time, yCrash, 'b:')
+plt.plot(mpc.time, mpc.x, 'r-', label='x')
+plt.plot(mpc.time, mpc.y, 'b-', label='y')
+plt.plot(mpc.time, mpc.vx, 'r.', label='vx')
+plt.plot(mpc.time, mpc.vy, 'b.', label='vy')
+# plt.plot(mpc.time, xCrash, 'r:')
+# plt.plot(mpc.time, yCrash, 'b:')
+plt.legend()
 plt.subplot(2, 3, 5)
-plt.plot(mpc.time, tCrash)
+plt.plot(mpc.time, mpc.θ_x, 'r-', label='theta_x')
+plt.plot(mpc.time, mpc.θ_y, 'b-', label='theta_y')
+plt.plot(mpc.time, mpc.w_x, 'r.', label='w_x')
+plt.plot(mpc.time, mpc.w_y, 'b.', label='w_y')
+plt.legend()
 plt.subplot(2, 3, 6)
-plt.plot(mpc.time, mpc.Gridx, 'r-')
-plt.plot(mpc.time, mpc.Gridy, 'b-')
+plt.plot(mpc.time, mpc.Gridx, 'r-', label='Gridx')
+plt.plot(mpc.time, mpc.Gridy, 'b-', label='Gridy')
+plt.legend()
 
 plt.show()

rocket/simulation.py
@@ -660,7 +660,7 @@ class MyApp(ShowBase):
     # Just set the estimator variables from our simulation
 
     mheVars = np.array([
-      x, y, z, vx, vy, vz, yaw * np.pi/180, pitch * np.pi/180, yawRate * np.pi/180, pitchRate * np.pi/180, prop
+      x, y, z, vx, vy, vz, yaw * np.pi/180, -pitch * np.pi/180, yawRate * np.pi/180, pitchRate * np.pi/180, prop
     ])
 
     print (mheVars)